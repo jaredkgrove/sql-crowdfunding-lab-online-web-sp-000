a0e00b0106785af2a51e88702bbb78e432cbd92c
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 19:30:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,8 +35,8 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, pledges.amount
-  FROM projects
-  INNER JOIN pledges
+  FROM pledges
+  INNER JOIN users
   ON projects.id = pledges.project_id
   GROUP BY projects.title
   HAVING SUM(pledges.amount) >= projects.funding_goal"
