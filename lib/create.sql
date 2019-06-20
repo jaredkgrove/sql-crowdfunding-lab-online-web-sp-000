5bb428b74b501ceef7c16ef0236ebf761da2771a
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 18:58:12 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,3 +5,17 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date TEXT
 );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+);
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT
+);
